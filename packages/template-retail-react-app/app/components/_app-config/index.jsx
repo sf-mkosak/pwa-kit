d190ee3f16422adb10d3569d8e11f3718f08b5bb
sf-mkosak <mkosak@salesforce.com>
2025-09-17 14:57:38 -0400

    reverted accidental commit to _app-config

--- packages/template-retail-react-app/app/components/_app-config/index.jsx
+++ packages/template-retail-react-app/app/components/_app-config/index.jsx
@@ -104,8 +104,8 @@ const AppConfig = ({children, locals = {}}) => {
             defaultDnt={DEFAULT_DNT_STATE}
             // Set 'enablePWAKitPrivateClient' to true to use SLAS private client login flows.
             // Make sure to also enable useSLASPrivateClient in ssr.js when enabling this setting.
-            enablePWAKitPrivateClient={true}
-            privateClientProxyEndpoint={slasPrivateClientProxyEndpoint}
+            enablePWAKitPrivateClient={false}
+            slasPrivateClientProxyEndpoint={slasPrivateClientProxyEndpoint}
             // Uncomment 'hybridAuthEnabled' if the current site has Hybrid Auth enabled. Do NOT set this flag for hybrid storefronts using Plugin SLAS.
             // hybridAuthEnabled={true}
             logger={createLogger({packageName: 'commerce-sdk-react'})}
