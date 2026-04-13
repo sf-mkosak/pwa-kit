89f9466129c1d80d7b78a4b7f2f01143acb4a279
Ben Chypak <bchypak@salesforce.com>
2026-04-13 16:36:58 -0700

    Lint

--- packages/pwa-kit-dev/jest.config.js
+++ packages/pwa-kit-dev/jest.config.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 const base = require('internal-lib-build/configs/jest/jest.config')
 
